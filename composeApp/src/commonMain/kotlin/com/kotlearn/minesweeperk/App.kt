679bd5aef635d1a3112f733f86a5266eb6999075
hogen <hogen.xue90@gmail.com>
2025-07-13 09:38:44 +0800

    1.Add modules

--- composeApp/src/commonMain/kotlin/com/kotlearn/minesweeperk/App.kt
+++ composeApp/src/commonMain/kotlin/com/kotlearn/minesweeperk/App.kt
@@ -1,17 +1,13 @@
 package com.kotlearn.minesweeperk
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import minesweeperk.composeapp.generated.resources.Res
-import minesweeperk.composeapp.generated.resources.compose_multiplatform
+import com.kotlearn.minesweeperk.feature.menu.MenuScreen
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -19,18 +15,11 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+        MenuScreen(
+            goToPlay = {},
+            goToSettings = {},
+            goToHighScores = {},
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
